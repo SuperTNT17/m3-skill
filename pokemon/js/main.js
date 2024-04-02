15afc8249f1e24b9d39d0f0c90dee4b01d8d7962
Robin Lemmers <38210@ma-web.nl>
2024-04-02 09:51:39 +0200

    pokemon code

--- pokemon/js/main.js
+++ pokemon/js/main.js
@@ -1,5 +1,6 @@
 const pokemonImage = document.getElementById("js--pokemon-image");
 let randomNumber = Math.floor(Math.random() * 1025 + 1);
+let pokemonName = "";
 let randomShiny = Math.floor(Math.random() * 8192);
 console.log(randomNumber);
 console.log(randomShiny)
@@ -11,6 +12,8 @@ let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber)
         return response.json();
     })
     .then(function(realData){
+        pokemonName = realData.name;
+        pokemonText.innerText = `A wild ${pokemonName} has appeared!`;
         if (randomShiny != 0) {
             pokemonImage.src = realData.sprites.front_default;
         }
@@ -24,16 +27,18 @@ const catchButton = document.getElementById("js--catch-button");
 const pokemonText = document.getElementById("js--pokemon-text");
 let pokemonGamePlayed = false;
 
+
+
 catchButton.onclick = function(){
     if (pokemonGamePlayed == false) {
         let catchNumber = Math.floor(Math.random() * 2);
         console.log(catchNumber);
         if(catchNumber == 0){
-            pokemonText.innerText = "The Pokémon ran away!";
+            pokemonText.innerText = `The ${pokemonName} ran away!`;
             pokemonText.style.color = "red";
         }
         else{
-            pokemonText.innerText = "The Pokémon has been caught!";
+            pokemonText.innerText = `The ${pokemonName} has been caught!`;
             pokemonText.style.color = "green";
         }
         pokemonGamePlayed = true;
