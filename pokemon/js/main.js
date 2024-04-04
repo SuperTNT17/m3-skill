a896ce68fa0750dcb0c925a14365da69301d57b4
Robin Lemmers <38210@ma-web.nl>
2024-04-04 09:02:21 +0200

    pokemon storage

--- pokemon/js/main.js
+++ pokemon/js/main.js
@@ -1,33 +1,35 @@
 const pokemonImage = document.getElementById("js--pokemon-image");
-let randomNumber = Math.floor(Math.random() * 1025 + 1);
+const pokemonStorageCard = document.getElementById("js--pokemon-storage");
+let randomNumber;
 let pokemonName = "";
-let randomShiny = Math.floor(Math.random() * 8192);
-console.log(randomNumber);
-console.log(randomShiny);
+let randomShiny;
+let pokemonStorage = [];
 
-
-let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(realData){
-        pokemonName = realData.name;
-        pokemonText.innerText = `A wild ${pokemonName} appeared!`;
-        if (randomShiny != 0) {
-            pokemonImage.src = realData.sprites.front_default;
-        }
-        else{
-            pokemonText.style.color = "orange";
-            pokemonImage.src = realData.sprites.front_shiny;
-        }
-    });
-
+pokemonGame();
+function pokemonGame(){
+    randomPokemon = Math.floor(Math.random() * 1025 + 1);
+    randomShiny = Math.floor(Math.random() * 8192);
+    let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/" + randomPokemon)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(realData){
+            pokemonName = realData.name;
+            pokemonText.innerText = `A wild ${pokemonName} appeared!`;
+            if (randomShiny != 0) {
+                pokemonImage.src = realData.sprites.front_default;
+            }
+            else{
+                pokemonText.style.color = "orange";
+                pokemonImage.src = realData.sprites.front_shiny;
+            }
+        });
+}
 
 const catchButton = document.getElementById("js--catch-button");
 const pokemonText = document.getElementById("js--pokemon-text");
+const againButton = document.getElementById("js--again-button");
 let pokemonGamePlayed = false;
-
-
 
 catchButton.onclick = function(){
     if (pokemonGamePlayed == false) {
@@ -38,9 +40,45 @@ catchButton.onclick = function(){
             pokemonText.style.color = "red";
         }
         else{
+            addPokemon(pokemonName);
             pokemonText.innerText = `The ${pokemonName} has been caught!`;
             pokemonText.style.color = "green";
         }
-        pokemonGamePlayed = true;
+        catchButton.disabled = true;
+        catchButton.style.display = "none";
+        againButton.style.display = "inline";
     }
+}
+
+againButton.onclick = function(){
+    againButton.style.display = "none";
+    catchButton.style.display = "inline";
+    catchButton.disabled = false;
+    pokemonText.style.color = "black";
+    pokemonGame();
+}
+
+function addPokemon(pokemonName){
+    pokemonStorage.push(pokemonName);
+    let pokemonItem = document.createElement("li");
+    let pokemonItemTitle = document.createElement("p");
+    pokemonItemTitle.innerHTML = pokemonName;
+    let pokemonItemImage = document.createElement("img");
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(realData){
+        if (randomShiny != 0) {
+            pokemonItemImage.src = realData.sprites.front_default;
+        }
+        else{
+            pokemonItemImage.src = realData.sprites.front_shiny;
+            pokemonItemTitle.style.color = "orange";
+        }
+    });
+    pokemonItem.appendChild(pokemonItemTitle);
+    pokemonItem.appendChild(pokemonItemImage);
+
+    pokemonStorageCard.appendChild(pokemonItem);
 }
